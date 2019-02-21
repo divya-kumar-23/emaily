5eb7a120910e8548064ed64a372bea3742a722f4
unknown <kumard2312@gmail.com>
2019-02-21 20:15:01 +0530

    v37

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 const express=require('express');
-const passport=require('passport');
-
-const GoogleStrategy=require('passport-google-oauth20').Strategy;
-const keys=require('./config/keys.js');
+const mongoose=require('mongoose');
+const keys=require('./config/keys');
+require('./services/passport');
+require('./models/User');
+const authRoutes=require('./routes/authRoutes');
+mongoose.connect(keys.mongoURI);
 const app=express();
 
-passport.use(new GoogleStrategy({
-    clientID:keys.googleClientID,
-    clientSecret:keys.googleClientSecret,
-    callbackURL:'/auth/google/callback'
-},(accessToken,refreshToken,profile,done)=>{
-    console.log(accessToken,refreshToken,profile,done)
-})
+authRoutes(app);
 
-);
-
-app.get('/auth/google',passport.authenticate('google',{
-    scope:['profile','email']
-})
-);
-
-
-app.get('/auth/google/callback',passport.authenticate('google',{
-    scope:['profile','email']
-})
-);
 //Client ID	
 // Client secret 
 const PORT=process.env.PORT
-app.listen(5000);        
+app.listen(5000);           
