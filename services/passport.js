42486498e855f2af5de58493e959a766e8a22539
unknown <kumard2312@gmail.com>
2019-02-21 20:33:05 +0530

    v38

--- services/passport.js
+++ services/passport.js
@@ -2,14 +2,17 @@
 const passport=require('passport');
 
 const GoogleStrategy=require('passport-google-oauth20').Strategy;
+const mongoose=require('mongoose');
 const keys=require('../config/keys.js');
 
+
+const User=mongoose.model('users');
 passport.use(new GoogleStrategy({
     clientID:keys.googleClientID,
     clientSecret:keys.googleClientSecret,
     callbackURL:'/auth/google/callback'
 },(accessToken,refreshToken,profile,done)=>{
-    console.log(accessToken,refreshToken,profile,done)
+   new User({googleId:profile.id}).save();
 })
 
 );
