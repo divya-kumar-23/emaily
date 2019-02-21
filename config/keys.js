dcab69a7babffe614107fbae778f33e76d80df93
unknown <kumard2312@gmail.com>
2019-02-21 18:16:05 +0530

    v27

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports={
     googleClientID:'1015557045470-gi7vqugoveq1496con2m90oa83j1ntqa.apps.googleusercontent.com',
     googleClientSecret:'W3TtGUcqrgK4BEzfHgRWlAzf'
-}
+};
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express=require('express');
 const passport=require('passport');
 
-const GoogleStrategy=require('passport-google-oath20').Strategy;
-const keys=require('./config/keys.js')
+const GoogleStrategy=require('passport-google-oauth20').Strategy;
+const keys=require('./config/keys.js');
 const app=express();
 
 passport.use(new GoogleStrategy({
-    clientID:keys.googleClintID,
+    clientID:keys.googleClientID,
     clientSecret:keys.googleClientSecret,
     callbackURL:'/auth/google/callback'
-},(accessToken)=>{
-
+},(accessToken,refreshToken,profile,done)=>{
+    console.log(accessToken,refreshToken,profile,done)
 })
 
 );
 
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+})
+);
+
+
+app.get('/auth/google/callback',passport.authenticate('google',{
+    scope:['profile','email']
+})
+);
 //Client ID	
 // Client secret 
 const PORT=process.env.PORT
