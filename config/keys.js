3a892f36b32cefdcf9b0f8263b3a648a5c299d16
divya kumar <divya.kumar@coxandkings.com>
2019-02-22 11:55:01 +0530

    Remove the now ignored keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    googleClientID:'1015557045470-gi7vqugoveq1496con2m90oa83j1ntqa.apps.googleusercontent.com',
-    googleClientSecret:'W3TtGUcqrgK4BEzfHgRWlAzf',
-    mongoURI:'mongodb://emaily:emaily0@ds137650.mlab.com:37650/emaily-devv',
-    cookieKey:'addawdawdawdawadawdawdwaddawadwwadawd'
-};
