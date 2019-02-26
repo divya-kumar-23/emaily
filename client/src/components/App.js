dfe7f66255645bd6582a38c625efad4f9c41bab7
unknown <kumard2312@gmail.com>
2019-02-27 00:32:32 +0530

    v73

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
 
+const Header=()=><h2>Header</h2>;
+const Dashboard=()=><h2>Dashboard</h2>;
+const SurweyNew=()=><h2>SurweyNew</h2>;
+const Landing=()=><h2>Landing</h2>;
 
 const App=()=> {
   return (
     <div>
-      hi there
+    <BrowserRouter>
+    <div>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/surveys' component={Dashboard} />
+        <Route path='/surveys/new' component={SurweyNew} />
+
     </div>
-  )
-}
+    </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
