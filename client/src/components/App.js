5d87e1172ebe0eff4e5418ed5c51acf1e67a5461
unknown <kumard2312@gmail.com>
2019-03-03 12:32:09 +0530

    v76

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter,Route} from 'react-router-dom';
 
-const Header=()=><h2>Header</h2>;
+import Header from './Header';
 const Dashboard=()=><h2>Dashboard</h2>;
 const SurweyNew=()=><h2>SurweyNew</h2>;
 const Landing=()=><h2>Landing</h2>;
@@ -11,11 +11,12 @@ const App=()=> {
     <div>
     <BrowserRouter>
     <div>
+        <Header/>
         <Route exact path='/' component={Landing} />
         <Route exact path='/surveys' component={Dashboard} />
         <Route path='/surveys/new' component={SurweyNew} />
 
-    </div>
+    </div>  
     </BrowserRouter>
     </div>
   );
